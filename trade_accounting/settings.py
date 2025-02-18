b63b8859b4a681c14183e0c95c686e5cbc96a207
EvgeniiZH <zharkovei@sstk.site>
2025-02-18 14:03:59 +0400

    Обновляю CSS

--- trade_accounting/settings.py
+++ trade_accounting/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Добавлено
     'django.contrib.sessions.middleware.SessionMiddleware',
     # 'django.middleware.locale.LocaleMiddleware',  # при необходимости
     'django.middleware.common.CommonMiddleware',
@@ -100,10 +101,10 @@ USE_TZ = True
 # Статические файлы
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Добавляем директорию для сбора статических файлов
 STATIC_ROOT = str(BASE_DIR / "staticfiles")
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

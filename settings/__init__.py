99f39728f60ca0bf552a1970c723361ba7bed790
EvgeniiZH <zharkovei@sstk.site>
2025-02-21 18:10:07 +0400

    Рабочая версия 1

--- settings/__init__.py
+++ settings/__init__.py
@@ -2,7 +2,7 @@
 import os
 
 # По умолчанию используем локальные настройки
-env = os.environ.get('DJANGO_ENV', 'local')
+env = os.environ.get('DJANGO_ENV', 'production')
 
 if env == 'production':
     from .production import *

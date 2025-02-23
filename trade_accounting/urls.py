29d3727edc93e4d7401a5c78a8eb4b802322b092
EvgeniiZH <zharkovei@sstk.site>
2025-02-23 14:11:43 +0400

    Добавлена тестовая ошибка для Sentry

--- trade_accounting/urls.py
+++ trade_accounting/urls.py
@@ -4,11 +4,16 @@ from django.template.context_processors import static
 from django.conf.urls.static import static
 from django.urls import path, include
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0  # Это вызовет ошибку 500
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('trades.urls')),  # Подключение приложения trades
+    path('sentry-debug/', trigger_error),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

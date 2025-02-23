8240fc943a4aa420cb55cf90d6b7f86e11cbd9a9
EvgeniiZH <zharkovei@sstk.site>
2025-02-23 11:06:34 +0400

    Рабочий проект со всеми изменениями

--- trade_accounting/settings/production.py
+++ trade_accounting/settings/production.py
@@ -3,15 +3,15 @@ from .base import *
 import os
 
 DEBUG = False
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '89.111.153.46,example.com').split(',')
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3-q)1+uqx7(8i(+6zg4+r^9ed4h%&5bmc%g%nm@w2-=$89ibr-')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django_project_db'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'me4ut9oodi3W'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST', 'db'),
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }

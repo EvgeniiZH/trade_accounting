e84ee98d734176c342c20d20c6cf2f53b2f615d8
EvgeniiZH <zharkovei@sstk.site>
2025-04-23 00:12:15 +0400

    🔧 Добавлен STATICFILES_DIRS в production.py

--- trade_accounting/settings/production.py
+++ trade_accounting/settings/production.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'kiteh.ru,www.kiteh.ru,89.111.153.46').split(',')
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-3-q)1+uqx7(8i(+6zg4+r^9ed4h%&5bmc%g%nm@w2-=$89ibr-')
+SECRET_KEY = os.getenv('SECRET_KEY', 'замени-на-настоящий')
 
 DATABASES = {
     'default': {
@@ -20,17 +20,11 @@ DATABASES = {
     }
 }
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-#
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
-
-
+STATIC_ROOT = '/var/www/trade_accounting/staticfiles'
+MEDIA_ROOT = '/var/www/trade_accounting/media'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://kiteh.ru",
@@ -38,23 +32,19 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 sentry_sdk.init(
-    dsn="https://1c5b83ea9bf786f67bddd04ef85e58d0@o4508867893526528.ingest.de.sentry.io/4508867895230544",
+    dsn=os.getenv("SENTRY_DSN", ""),
     integrations=[DjangoIntegration()],
-    send_default_pii=True,  # Сбор информации о пользователях (по желанию)
-    traces_sample_rate=1.0,  # 100% запросов будет отслеживаться (можно снизить)
-    _experiments={
-        "continuous_profiling_auto_start": True,  # Включаем автоматическое профилирование
-    },
+    send_default_pii=True,
+    traces_sample_rate=1.0,
+    _experiments={"continuous_profiling_auto_start": True},
 )
 
-# Настройка кеширования через Redis
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://redis:6379/1",  # Подключение к Redis-контейнеру
+        "LOCATION": "redis://redis:6379/1",
     }
 }
 
-# Используем кеш-сессии вместо базы данных (ускоряет работу)
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
